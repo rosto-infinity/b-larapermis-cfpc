73c16c703533f860cc9f997d455bfba2ea473657
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-24 16:05:47 +0100

    template index-permission

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('permissions', PermissionController::class);
 
 Route::get('/', function () {
     return view('welcome');
